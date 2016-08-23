fbca65f268d83564ced5826f95b157b6e3ad2a63
Ashley Felton <ashley@ropable.com>
2016-08-23 13:45:27 +0800

    Gigasecond reference.

--- exercism/rust/gigasecond/src/lib.rs
+++ exercism/rust/gigasecond/src/lib.rs
@@ -3,6 +3,7 @@ use chrono::*;
 
 pub fn after(start_date: DateTime<UTC>) -> DateTime<UTC> {
     // This function accepts and returns a UTC DateTime.
+    // Ref: https://lifthrasiir.github.io/rust-chrono/chrono/index.html
     let x: i64 = 1000000000;
     let result = start_date + Duration::seconds(x);
     result
